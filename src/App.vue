7b1eaafb3712281638b30c1b52ee007e1996c87e
elonehoo <hi@elonehoo.me>
2023-08-23 17:02:36 +0800

    feat: init core

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: import.meta.env.VITE_APP_TITLE,
+})
+</script>
+
+<template>
+  <main font-sans p="x-4 y-10">
+    <RouterView />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Vditor from 'vditor';
+import 'vditor/dist/index.css';
+
+const vditor = ref<Vditor | null>(null);
+
+onMounted(() => {
+  vditor.value = new Vditor('vditor', {
+    width:'50%',
+    typewriterMode:true,
+    mode:'ir',
+    toolbarConfig:{
+      hide: true,
+    },
+    preview:{
+      hljs:{
+        style: 'github-dark'
+      }
+    },
+    after: () => {
+      // vditor.value is a instance of Vditor now and thus can be safely used here
+      vditor.value!.setValue("# Vue Composition API + Vditor + TypeScript Minimal Example");
+    },
+  });
+});
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <div id="vditor" />
+  </div>
+
+</template>
