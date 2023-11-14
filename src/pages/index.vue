af7bffdb70a0dc5606d29863197bbb66837dbdd5
elonehoo <hi@elonehoo.me>
2023-11-14 19:09:23 +0800

    feat: add eslint config

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,35 +1,34 @@
 <script setup lang="ts">
-import Vditor from 'vditor';
-import 'vditor/dist/index.css';
+import Vditor from 'vditor'
+import 'vditor/dist/index.css'
 
-const vditor = ref<Vditor | null>(null);
+const vditor = ref<Vditor | null>(null)
 
 onMounted(() => {
   vditor.value = new Vditor('vditor', {
-    width:'50%',
-    typewriterMode:true,
-    mode:'ir',
-    toolbarConfig:{
+    width: '50%',
+    typewriterMode: true,
+    mode: 'ir',
+    toolbarConfig: {
       hide: true,
     },
-    preview:{
-      hljs:{
+    preview: {
+      hljs: {
         defaultLang: 'javascript',
-        style: 'github-dark'
-      }
+        style: 'github-dark',
+      },
     },
     after: () => {
       // vditor.value is a instance of Vditor now and thus can be safely used here
-      vditor.value!.setValue("**Hey, welcome to writer.**");
+      vditor.value!.setValue('**Hey, welcome to writer.**')
       vditor.value?.focus()
     },
-  });
-});
+  })
+})
 </script>
 
 <template>
   <div class="flex justify-center items-center">
     <div id="vditor" />
   </div>
-
 </template>
